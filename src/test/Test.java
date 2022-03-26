d0a07fc1dbcf8a0870251e34406a8562f52653f3
Glen R. Vasa <glenvasa@gmail.com>
2022-03-26 12:09:16 -0400

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,17 +4,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Person glen = new Person("Glen", 46, 184.5);
-		Person ethan = new Person("Ethan", 9, 80.0);
-		Person[] vasa = {ethan, glen};
+		Person russell = new Person("Russell", 46, 184.5);
+		Person andrew = new Person("Andrew", 9, 80.0);
+		Person samuel = new Person("Samuel", 13, 132.8);
+		Person[] asav = {andrew, samuel, russell};
 		
-		printNames(vasa);
+		printNames(asav);
 		
 	}
 	
 	public static void printNames(Person[] people) {
 		for(Person person : people) {
-			System.out.println(person.name);
+			System.out.println(person.name + " is "+person.age+ " years old" + " and weighs "+ person.weight+ " pounds.");
 		}
 	}
 
